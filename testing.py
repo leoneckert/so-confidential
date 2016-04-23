fea48e6d483af63cdc66667c82b188a820084a22
Leon Eckert <eckertleon@gmail.com>
2016-04-22 22:33:08 -0400

    buidling data structure

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
 import operator
+from pprint import pprint
 
 
 # from: http://stackoverflow.com/a/32988831/6163933
@@ -16,68 +17,72 @@ msgs = dict()
 
 if __name__ == "__main__":
    
- 
-	# wordsTally = dict()
-	# for line in os_system('sqlite3 ~/Library/Messages/chat.db "select text from message where is_from_me=1;"'):
-	# 	line = line.strip()
-	# 	words = line.split()
-	# 	# print words
-	# 	for word in words:
-	# 		if word not in wordsTally:
-	# 			wordsTally[word] = 0
-	# 		wordsTally[word] += 1
-
-
-	# SortedwordsTally2 = sorted(wordsTally.items(), key=lambda x: (-x[1], x[0]))
-	# for i in range(200):
-	# 	w = SortedwordsTally2[i][0]
-	# 	print w, " ",
-
-	# print "\n"*5
-
-
-	# wordsTally = dict()
-	# for line in os_system('sqlite3 ~/Library/Messages/chat.db "select text from message;"'):
-	# 	line = line.strip()
-	# 	words = line.split()
-	# 	# print words
-	# 	for word in words:
-	# 		if word not in wordsTally:
-	# 			wordsTally[word] = 0
-	# 		wordsTally[word] += 1
-
-
-	# SortedwordsTally2 = sorted(wordsTally.items(), key=lambda x: (-x[1], x[0]))
-	# for i in range(200):
-	# 	w = SortedwordsTally2[i][0] + " " + str(SortedwordsTally2[i][1])
-	# 	print w, " ",
-
 	handle_id = 0;
 	zeroSequence = 0
+
 	while True:
 		msg_count = 0
 		# print "trying handle_id: " + str(handle_id)
 		
+		currentConvo = 0;
+		convoELem = 0
 		for line in os_system('sqlite3 ~/Library/Messages/chat.db "select handle_id, is_from_me, date, text from message where handle_id='+ str(handle_id) + ';"'):
 			line = line.strip()
-			# print line
-			msg_count += 1;
+			# prints lines like this: 0|1|451686174|yes, thanks of the quick reply
+			elems = line.split("|")
+			# prints data like this: ['0', '1', '451686174', 'yes, thanks of the quick reply']
+		
+			if len(elems) > 4:
+				print "NOOOOOOOOOT THE LENGHT EXPECTED: " + str(len(elems))
+				print elems
+				while len(elems) > 4:
+					elems[len(elems) - 2] =  elems[len(elems) - 2] + "|" + elems[len(elems) - 1]
+					elems.remove(elems[len(elems) - 1])
+					print elems
+
+			if len(elems) == 4:
+				person = elems[0]
+				speaker = elems[1]
+				time = elems[2]
+				text = elems[3]
+
+				if person not in msgs:
+					msgs[person] = dict()
+
+				if currentConvo not in msgs[person]:
+					msgs[person][currentConvo] = list()
+				
+				tempData = list()
+				tempData.append(text)
+				tempData.append(speaker)
+				tempData.append(time)
+				msgs[person][currentConvo].append(tempData)
+				convoELem += 1
+			elif len(elems) == 1:
+				msgs[person][currentConvo][convoELem - 1][0] = msgs[person][currentConvo][convoELem -1][0] + " " + elems[0]
+			else:
+				print "NOOOOOOOOOT THE LENGHT EXPECTED: " + str(len(elems))
+				print elems
+				print "-"*40
+			
+			msg_count += 1
+		
+		# break
 		if msg_count > 0:
 			zeroSequence = 0
 		elif msg_count == 0:
 			zeroSequence += 1
+
 		if zeroSequence > 20:
 			print "reached handle_id " + str(handle_id) + " and counted 20 handle_id in a row with no messages. We must be done."
 			break
 
-
-			
-		# print str(handle_id) + " : " +  str(msg_count) + " messages"
 		handle_id += 1
 
 
 
 	print "done"
+	pprint(msgs)
 
 
 
