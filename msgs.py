7f734695a915bfc27b6a8df2df31ee7196c3f902
Leon Eckert <eckertleon@gmail.com>
2016-04-25 16:32:31 -0400

    playing around

--- msgs.py
+++ msgs.py
@@ -2,6 +2,7 @@ import operator
 from textblob import TextBlob, Word
 
 def wordTally(db):
+	# use like this: word_tally = msgs.wordTally(db)
 	allwords = dict()
 
 	for person in db:
@@ -18,6 +19,10 @@ def wordTally(db):
 	return allwords
 
 def orderTally(tally):
+	""" use like this:
+	word_tally = msgs.wordTally(db)
+	word_tally = msgs.orderTally(word_tally) """
+
 	output = list()
 	SortedwordsTally2 = sorted(tally.items(), key=lambda x: (-x[1], x[0]))
 	for i in SortedwordsTally2:
@@ -28,3 +33,110 @@ def orderTally(tally):
 	
 	return output
 		
+def returnAllTexts(db):
+	""" use like this:
+	all_msgs = msgs.returnAllTexts(db)
+	print all_msgs """
+
+	outputLines = ""
+ 	for person in db:
+		for convo in db[person]:
+			for text_data in db[person][convo]:
+				if(len(text_data[0]) > 0):
+					outputLines = outputLines + text_data[0] + "\n"
+	return outputLines		
+
+# def getNumConvos(db):
+# 	count = 0
+# 	for person in db:
+# 		for convo in db[person]:
+
+
+def returnRandomConvo(db):
+	outputLines = ""
+	randomPerson = random.sample(db, 1)
+	# print "random person: " + str(randomPerson)
+	randomConvo = random.sample(db[randomPerson[0]], 1)
+	# print "random convo: " + str(randomConvo) 
+	for elem in db[randomPerson[0]][randomConvo[0]]:
+		outputLines = outputLines + elem[0] + "\n"
+	return outputLines
+
+def returnRandomText(db):
+	outputLines = ""
+	randomPerson = random.sample(db, 1)
+	# print "random person: " + str(randomPerson)
+	randomConvo = random.sample(db[randomPerson[0]], 1)
+	# print "random convo: " + str(randomConvo) 
+	randomElem = random.sample(db[randomPerson[0]][randomConvo[0]], 1)
+	return randomElem[0][0]
+
+
+
+
+
+
+
+
+
+
+
+
+#from here: https://github.com/aparrish/rwet-examples/blob/master/ngrams/ngramcount.py
+def count_ngrams(tokens, n):
+	"""Returns a dictionary mapping tuples of n-grams with order n to the number
+		of times that n-gram occurred in list tokens."""
+	ngrams = dict()
+	if len(tokens) < n:
+		return ngrams
+	for i in range(len(tokens) - n + 1):
+		ngram = tuple(tokens[i:i+n])
+		if ngram not in ngrams:
+			ngrams[ngram] = 0
+		ngrams[ngram] += 1
+	return ngrams
+
+#from here: https://github.com/aparrish/rwet-examples/blob/master/ngrams/ngramcount.py
+def build_model(tokens, n):
+	"Builds a Markov model from the list of tokens, using n-grams of length n."
+	model = dict()
+	if len(tokens) < n:
+		return model
+	for i in range(len(tokens) - n):
+		gram = tuple(tokens[i:i+n])
+		next_token = tokens[i+n]
+		if gram in model:
+			model[gram].append(next_token)
+		else:
+			model[gram] = [next_token]
+	final_gram = tuple(tokens[len(tokens)-n:])
+	if final_gram in model:
+		model[final_gram].append(None)
+	else:
+		model[final_gram] = [None]
+	return model
+
+import random
+def generate(model, n, seed=None, max_iterations=100):
+	"""Generates a list of tokens from information in model, using n as the
+		length of n-grams in the model. Starts the generation with the n-gram
+		given as seed. If more than max_iteration iterations are reached, the
+		process is stopped. (This is to prevent infinite loops)""" 
+	if seed is None:
+		seed = random.choice(model.keys())
+	output = list(seed)
+	current = tuple(seed)
+	for i in range(max_iterations):
+		if current in model:
+			possible_next_tokens = model[current]
+			next_token = random.choice(possible_next_tokens)
+			if next_token is None: break
+			output.append(next_token)
+			current = tuple(output[-n:])
+		else:
+			break
+	return output
+
+
+
+
--- testing.py
+++ testing.py
@@ -1,6 +1,5 @@
-
-# import mdb
-# import msgs
+import mdb
+import msgs
 from pprint import pprint
 
 
@@ -18,17 +17,41 @@ mdb.init(convoInterval)
 
 if __name__ == "__main__" and len(mdb.db()) > 0:
 	
-
-
-
-
 	print "[+] actual program starts.\n"
 	db = mdb.db()
-	# pprint(db)
-	
-	word_tally = msgs.wordTally(db)
-	pprint(word_tally)
-	for i in range(500):
-		print word_tally[i][0]
-   
-	
+
+	all_msgs = msgs.returnAllTexts(db)
+	print all_msgs
+	# for i in range(10):
+	# 	print msgs.returnRandomText(db)
+	# 	print "-"*50
+	# for u in range(30):
+	# 	text = " "
+	# 	while text[-1] is not "?":
+	# 		text = msgs.returnRandomText(db)
+	# 		if len(text) < 1:
+	# 			text = " "
+	# 	print text
+	# 	print "-"*50
+
+
+	# r_convo = returnRandomConvo(db)
+	# print r_convo
+
+
+	# all_message_string = ""
+	# for msg in all_msgs:
+	# 	all_message_string = all_message_string + " " + msg
+	# print all_message_string
+
+
+
+
+
+
+
+
+
+
+
+
