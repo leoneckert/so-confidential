5a2275f98cae4fbf4fc146daaf85237decb549ea
Leon Eckert <eckertleon@gmail.com>
2016-05-06 17:55:10 -0400

    smaller random values

--- v3.py
+++ v3.py
@@ -11,20 +11,14 @@ RECEIVED_sentences = mdb.getAllSentences(speaker=0, eng_filter=1)
 
 
 SENTdata = mdb.getTrendingWordsAndSentences(SENT_sentences, days_per_segment = 5, num_words = 2, max_sent_length = 40, blacklist_freq = 0.7)
+RECEIVEDdata = mdb.getTrendingWordsAndSentences(RECEIVED_sentences, days_per_segment = 5, num_words = 2, max_sent_length = 40, blacklist_freq = 0.7)
 # returns dict with these objects:
 	# populated_indeces
 	# word_index
 	# index_date
 	# index_word_sentence
 	# index_num_sentences
-RECEIVEDdata = mdb.getTrendingWordsAndSentences(RECEIVED_sentences, days_per_segment = 5, num_words = 2, max_sent_length = 40, blacklist_freq = 0.7)
-# for s in SENTdata:
-# 	print s
 
-# commonIndeces = msgs.getCommonIndeces(RECEIVEDdata["populated_indeces"], SENTdata["populated_indeces"])
-# pprint(commonIndeces)
-
-poetryout.generateTimebasedPoetry(SENTdata, RECEIVEDdata, numPeriods = 3, numSentPerPersonPerPeriod = 2)
 
 
 
@@ -32,12 +26,8 @@ firstRun = True
 while True:
 	if firstRun == True or raw_input('\n\nPress enter for another poem...\n\n') == '':
 		firstRun = False
-		poetryout.generateTimebasedPoetry(SENTdata, RECEIVEDdata, numPeriods = random.choice(range(1,5)), numSentPerPersonPerPeriod = random.choice(range(1,5)))
-
-
-
-
-# for i in range(100):
-# 	poetryout.generateTimebasedPoetry(SENTdata, RECEIVEDdata, numPeriods = random.choice(range(1,5)), numSentPerPersonPerPeriod = random.choice(range(1,5)))
+		np = random.choice(range(1,3))
+		nspppp = random.choice(range(1,3))
+		poetryout.generateTimebasedPoetry(SENTdata, RECEIVEDdata, numPeriods = np, numSentPerPersonPerPeriod = nspppp)
 
 
